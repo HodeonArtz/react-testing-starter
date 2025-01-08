babc786d3e9d868d868a0dbc1f7b7a04436c86b3
Glen Marti <martidicianolold@gmail.com>
2025-01-08 08:51:40 +0100

    test(OrderStatusSelector): test that values option values are "new", "processed" and "fulfilled"

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -5,9 +5,10 @@ import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
   const renderComponent = () => {
+    const onChange = vi.fn();
     render(
       <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
+        <OrderStatusSelector onChange={onChange} />
       </Theme>
     );
     const button = screen.getByRole("combobox");
@@ -16,11 +17,16 @@ describe("OrderStatusSelector", () => {
     async function getOptions() {
       return await screen.findAllByRole("option");
     }
+    async function getOption(value: RegExp) {
+      return await screen.findByRole("option", { name: value });
+    }
 
     return {
       button,
       user,
       getOptions,
+      getOption,
+      onChange,
     };
   };
 
@@ -39,5 +45,32 @@ describe("OrderStatusSelector", () => {
     expect(options).toHaveLength(3);
     const labels = options.map((option) => option.textContent);
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
+  });
+
+  it.each([
+    { label: /processed/i, value: "processed" },
+    { label: /fulfilled/i, value: "fulfilled" },
+  ])("should render with $label value", async ({ label, value }) => {
+    const { button, user, onChange } = renderComponent();
+    await user.click(button);
+
+    const option = await screen.findByRole("option", { name: label });
+    await user.click(option);
+    expect(onChange).toHaveBeenCalledWith(value);
+  });
+  it("should render with new value", async () => {
+    const { button, user, onChange } = renderComponent();
+    const label = /new/i;
+    const value = "new";
+    await user.click(button);
+    const processedOption = await screen.findByRole("option", {
+      name: /processed/i,
+    });
+    await user.click(processedOption);
+
+    await user.click(button);
+    const option = await screen.findByRole("option", { name: label });
+    await user.click(option);
+    expect(onChange).toHaveBeenCalledWith(value);
   });
 });
