0c4047351ffb37a0e5c525940c96962eb9e3e628
Glen Marti <martidicianolold@gmail.com>
2025-01-10 09:33:50 +0100

    test(toaster)

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -4,6 +4,7 @@ import ProductForm from "../../src/components/ProductForm";
 import { Category, Product } from "../../src/entities";
 import { AllProviders } from "../AllProviders";
 import { db } from "../mocks/db";
+import { Toaster } from "react-hot-toast";
 
 describe("ProductForm", () => {
   let category: Category;
@@ -16,24 +17,69 @@ describe("ProductForm", () => {
   });
 
   const renderComponent = (product?: Product) => {
+    const onSubmit = vi.fn();
+
     render(
       <AllProviders>
-        <ProductForm product={product} onSubmit={vi.fn()} />
+        <ProductForm product={product} onSubmit={onSubmit} />
+        <Toaster />
       </AllProviders>
     );
 
     const waitForFormToLoad = async () => {
       await screen.findByRole("form");
+
+      const nameField = screen.getByPlaceholderText(/name/i),
+        priceField = screen.getByPlaceholderText(/price/i),
+        comboBoxField = screen.getByRole("combobox", { name: /category/i }),
+        submitButton = screen.getByRole("button");
+
+      type FormData = {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        [K in keyof Product]: any;
+      };
+
+      const validData: FormData = {
+        id: 1,
+        name: "a",
+        price: 1,
+        categoryId: category.id,
+      };
+
+      const fill = async (product: FormData) => {
+        const user = userEvent.setup();
+
+        if (product.name !== undefined)
+          await user.type(nameField, product.name);
+
+        if (product.price !== undefined)
+          await user.type(priceField, product.price + "");
+
+        await user.tab();
+        await user.click(comboBoxField);
+        const options = screen.getAllByRole("option");
+        await user.click(options[0]);
+        await user.click(submitButton);
+      };
+
       return {
-        nameField: screen.getByPlaceholderText(/name/i),
-        priceField: screen.getByPlaceholderText(/price/i),
-        comboBoxField: screen.getByRole("combobox", { name: /category/i }),
-        submitButton: screen.getByRole("button"),
+        nameField,
+        priceField,
+        comboBoxField,
+        submitButton,
+        fill,
+        validData,
       };
     };
 
     return {
       waitForFormToLoad,
+      expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
+        const error = screen.getByRole("alert");
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveTextContent(errorMessage);
+      },
+      onSubmit,
     };
   };
 
@@ -69,18 +115,73 @@ describe("ProductForm", () => {
     expect(nameField).toHaveFocus();
   });
 
-  it("should display an error if name is missing", async () => {
-    const { waitForFormToLoad } = renderComponent();
-    const form = await waitForFormToLoad();
-    const user = userEvent.setup();
-    await user.type(form.priceField, "10");
-    await user.click(form.comboBoxField);
-    const options = screen.getAllByRole("option");
-    await user.click(options[0]);
-    await user.click(form.submitButton);
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "less than 1",
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenario: "more than 1000",
+      price: 1001,
+      errorMessage: /1000/,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
+      const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, price });
 
-    const error = screen.getByRole("alert");
-    expect(error).toBeInTheDocument();
-    expect(error).toHaveTextContent(/required/i);
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "longer than 255",
+      name: "a".repeat(256),
+      errorMessage: /255/,
+    },
+  ])(
+    "should display an error if name is $scenario",
+    async ({ name, errorMessage }) => {
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
+      const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, name });
+
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
+
+  it("should call onSubmit with the correct data", async () => {
+    const { waitForFormToLoad, onSubmit } = renderComponent();
+    const form = await waitForFormToLoad();
+    await form.fill({ ...form.validData });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...formData } = form.validData;
+    expect(onSubmit).toHaveBeenCalledWith(formData);
+  });
+  it("should show toast message on submit error", async () => {
+    const { waitForFormToLoad, onSubmit } = renderComponent();
+    onSubmit.mockRejectedValue({});
+    const form = await waitForFormToLoad();
+    await form.fill({ ...form.validData });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+    const toast = await screen.findByRole("status");
+    expect(toast).toBeInTheDocument();
+    expect(toast).toHaveTextContent(/error/i);
   });
 });
